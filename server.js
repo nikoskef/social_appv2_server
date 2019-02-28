df2f808afd6c83817fb8a14bc7a6aa066c378be8
nikoskef <nikoskef86@gmail.com>
2019-02-28 19:08:26 +0200

    helmet, compression

--- server.js
+++ server.js
@@ -2,17 +2,12 @@ const express = require("express");
 const logger = require("./middleware/logger");
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
+require("./startup/cors")(app);
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 5000;
 
