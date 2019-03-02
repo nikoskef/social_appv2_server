566a3e43b381d389b23f79e3fc6381733e0d57ad
nikoskef <nikoskef86@gmail.com>
2019-03-02 14:21:18 +0200

    serveless fixed, un bcrypt, i bcryptjs

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const gravatar = require("gravatar");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { User } = require("../models/User");
 
 exports.getAuthTest = (req, res) => {
